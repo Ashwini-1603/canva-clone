d17c718ac78c366d05e827f246b1293a3c72706d
Ashwini-1603 <106587281+Ashwini-1603@users.noreply.github.com>
2023-09-05 11:40:54 +0530

    Update deploy.sh
    added

--- deploy.sh
+++ deploy.sh
@@ -20,7 +20,7 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:Ashwini-1603/canva2.git main:gh-pages
+# git push -f git@github.com:Ashwini-1603/canva-clone.git main:gh-pages
 
 cd -
 
